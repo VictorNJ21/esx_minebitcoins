8afeac39a2bfca9fd9c8ad6de7efb5793e642d10
VictorNJ21 <65968706+VictorNJ21@users.noreply.github.com>
2020-08-13 11:57:54 +0200

    Add files via upload

---
+++ locales/no.lua
@@ -0,0 +1,22 @@
+Locales['no'] = {
+    ['mine_bitcoins'] = 'Trykk [~r~E~w~] for å samle bitcoins.',
+    ['stop_bitcoins'] = 'Trykk [~r~E~w~] for å stoppe å samle bitcoins.',
+    ['sell_bitcoins'] = 'Trykk [~r~E~w~] to for å selge bitcoins.',
+    ['putting_bitcoins'] = 'Legger bitcoins ut for salg...',
+
+    ['notify_title'] = 'LSPD',
+    ['notify_subject'] = 'Melding',
+    ['notify_msg_sell'] = 'Mistenkt har blitt sett selge ulovlig oppnådd bitcoins i lava fabrikken.',
+    ['notify_msg_mine'] = 'Mistenkt har blitt sett hacke bitcoins i Lesters hus.',
+
+    ['mine_blip'] = 'Bitcoins | Samle',
+    ['sell_blip'] = 'Bitcoins | Selge',
+
+    ['need_raspberry'] = 'Du trenger en raspberry!',
+    ['no_space'] = 'Du har ikke nok plass!',
+    ['no_bitcoins'] = 'Du har ikke noen bitcoins!',
+
+    ['sold_part_1'] = 'Du solgte ~y~x',
+    ['sold_part_2'] = ' ~b~bitcoins~w~ for~r~ $',
+    ['sold_part_3'] = ' svarte penger~w~!',
+}
---
+++ server/main.lua
@@ -0,0 +1,49 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterNetEvent('esx_minebitcoins:CheckIfHasRaspberry')
+AddEventHandler('esx_minebitcoins:CheckIfHasRaspberry', function(xPlayer)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    if xPlayer.getInventoryItem('raspberry').count >= 1 then
+        TriggerClientEvent('esx_minebitcoins:HasRaspberry', source)
+        if Config.RemoveRaspberry then
+            xPlayer.removeInventoryItem('raspberry', 1)
+        end
+    else
+        xPlayer.showNotification(_U('need_raspberry'))
+    end
+end)
+
+RegisterNetEvent('esx_minebitcoins:RecieveBitcoins')
+AddEventHandler('esx_minebitcoins:RecieveBitcoins', function(xPlayer)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    if xPlayer.canCarryItem('bitcoin', 1) then
+        xPlayer.addInventoryItem('bitcoin', 1)
+    else
+        xPlayer.showNotification(_U('no_space'))
+    end
+end)
+
+RegisterNetEvent('esx_minebitcoins:SellBitcoins')
+AddEventHandler('esx_minebitcoins:SellBitcoins', function(xPlayer)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local amount = xPlayer.getInventoryItem('bitcoin').count
+    local reward = Config.SellReward * amount
+    if amount >= 1 then
+        TriggerClientEvent('esx_minebitcoins:HasBitcoins', source)
+        Citizen.Wait(Config.SellDuration)
+        xPlayer.removeInventoryItem('bitcoin', amount)
+        if Config.GiveDirtMoney then
+            xPlayer.addAccountMoney('black_money', reward)
+        else
+            xPlayer.addMoney(reward)
+        end
+        xPlayer.showNotification(_U('sold_part_1') .. amount .. _U('sold_part_2') .. reward .. _U('sold_part_3'))
+    else
+        xPlayer.showNotification(_U('no_bitcoins'))
+    end
+end)
